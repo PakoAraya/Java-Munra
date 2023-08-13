49490d8b5596afb8961351510a53610a9cad79b6
Francisco Javier Araya Hernandez <franarayah@gmail.com>
2023-08-13 18:54:20 -0400

    Se agrega la clase persona y se instancia de clase Main, Commit desde Sourcetree

--- Tutorial_001_Basic/src/main/Application.java
+++ Tutorial_001_Basic/src/main/Application.java
@@ -2,6 +2,9 @@ package main;
 
 public class Application {
 	public static void main(String[] args) {
-    System.out.println("Ahora si que si");		
+		System.out.println("Ahora si que si con Java Java Du!!!");
+		System.out.println("Java con versionamiento en GitHub");
+		
+		Persona persona = new Persona();
 	}
 }
