07ce093fab86bb6a1ba454063530ec2b65e66c3c
Francisco Javier Araya Hernandez <franarayah@gmail.com>
2023-09-26 13:56:00 -0300

    Se registra ciclo de control for

---
+++ Tutorial_004_Basic/src/main/TablaFor.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Iterator;
+
+public class TablaFor {
+	public static void main(String[] args) {
+		int tabla7 = 7, resultado;
+		for (int i=0; i <= 10; i++) {
+			resultado = tabla7 * i;
+			System.out.println("El resultado de 7 x " + " es: " + resultado );
+		}
+	System.out.println("Process Completed!");
+	}
+}
